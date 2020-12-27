e82455a06787aaad2a92b3d7b593de75af77dda1
Shane Vitarana <shanev@gmail.com>
2020-12-26 19:46:07 -0500

    reproduced 'unsupported tendermint version'

--- packages/api/src/api.spec.ts
+++ packages/api/src/api.spec.ts
@@ -8,7 +8,7 @@ describe('StakebirdApi', () => {
 		api = await StakebirdApi.connect({
 			connection: {
 				type: 'tendermint',
-				url: 'http://devnet.stakebird.com:26657',
+				url: 'localhost:26657',
 			},
 		});
 	});
@@ -16,7 +16,7 @@ describe('StakebirdApi', () => {
 	it('should fetch balances using tendermint client', async () => {
 		const impl = new QueryClientImpl(api.connection.queryConnection);
 		const res = await impl.AllBalances({
-			address: 'regen:1j9h8dpu7ah2hl9rg7ycu0e64kh90rrlpk9kagz', // Amaury's account.
+			address: 'stb1nnz4naa7f0z6pwga3xkvcn3ju0unclm700rqpe', // Shane's account.
 		});
 
 		// TODO So ideally, the two lines would be combined into one:
