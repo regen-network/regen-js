0798b1cd672aa11d2410de8c8b634ba1877c4d35
Amaury <amaury.martiny@protonmail.com>
2021-01-14 11:30:44 +0100

    fix(api): Regenerate from v0.40.0 proto files (#13)

--- packages/api/scripts/protocgen.sh
+++ packages/api/scripts/protocgen.sh
@@ -7,9 +7,9 @@ PROTO_DIRS=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1
 
 # Generate TS files with ts-proto
 for dir in $PROTO_DIRS; do
-  protoc \
+  buf protoc \
     -I "proto" \
-    -I "proto/third_party" \
+    -I "third_party/proto" \
     --plugin=${TS_PROTO_BIN} \
     --ts_proto_opt=useOptionals=true \
     --ts_proto_opt=forceLong=long \
