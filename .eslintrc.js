b52e41527be67582163dffd46240a609d2c8ba6b
Mark Hagelberg <mark.hagelberg@gmail.com>
2022-01-18 13:20:22 -0700

    - remove amaury/eslintrc - copy eslint config form regen-web

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,22 @@
 module.exports = {
-	...require('@amaurym/eslintrc'),
 	parserOptions: {
 		project: './tsconfig.json',
+	},
+	extends: [
+		'react-app',
+		'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+		'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+	],
+	rules: {
+	'no-empty': 'off',
+	'no-console': 'warn',
+	'no-useless-escape': 'off',
+	'@typescript-eslint/explicit-function-return-type': [
+			'error',
+			{
+				allowExpressions: true,
+			},
+		],
+	'@typescript-eslint/no-explicit-any': 'off',
 	},
 };
