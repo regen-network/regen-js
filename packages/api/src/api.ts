dd2c1dfa16817214c764dc7e1908643ea3f3aa06
Mark Hagelberg <mark.hagelberg@gmail.com>
2022-01-18 13:20:22 -0700

    - remove amaury/eslintrc - copy eslint config form regen-web
    ran yarn lint --fix
    
    resolve lint
    
    use Hambach in test
    
    copy .prettierrc.json from regen-web
    
    run --fix again to use single quotes
    
    use latest configs from regen-web dependency upgrades

--- packages/api/src/api.ts
+++ packages/api/src/api.ts
@@ -3,15 +3,15 @@ import { adaptor34, Client as TendermintClient } from '@cosmjs/tendermint-rpc';
 import { Connection, createTendermintConnection } from './connection';
 
 interface TendermintConnection {
-	type: 'tendermint';
-	url: string;
+  type: 'tendermint';
+  url: string;
 }
 
 /**
  * Options to pass into the RegenApi constructor.
  */
 export interface RegenApiOptions {
-	connection: TendermintConnection;
+  connection: TendermintConnection;
 }
 
 /**
@@ -19,32 +19,30 @@ export interface RegenApiOptions {
  * a client connection
  */
 export class RegenApi {
-	readonly connection: Connection;
+  readonly connection: Connection;
 
-	constructor(connection: Connection) {
-		this.connection = connection;
-	}
+  constructor(connection: Connection) {
+    this.connection = connection;
+  }
 
-	/**
-	 * Create a RegenApi object which connects to the given gRPC connection.
-	 *
-	 * @param options - Options to pass into RegenAPI.
-	 */
-	public static async connect(options: RegenApiOptions): Promise<RegenApi> {
-		switch (options.connection.type) {
-			case 'tendermint': {
-				const tendermintClient = await TendermintClient.connect(
-					options.connection.url,
-					// Since v0.40.0, CosmJS cannot detect the Tendermint
-					// version used by the node. Here, we just hardcode to use
-					// an adaptor for Tendermint 0.34.x.
-					adaptor34
-				);
+  /**
+   * Create a RegenApi object which connects to the given gRPC connection.
+   *
+   * @param options - Options to pass into RegenAPI.
+   */
+  public static async connect(options: RegenApiOptions): Promise<RegenApi> {
+    switch (options.connection.type) {
+      case 'tendermint': {
+        const tendermintClient = await TendermintClient.connect(
+          options.connection.url,
+          // Since v0.40.0, CosmJS cannot detect the Tendermint
+          // version used by the node. Here, we just hardcode to use
+          // an adaptor for Tendermint 0.34.x.
+          adaptor34,
+        );
 
-				return new RegenApi(
-					createTendermintConnection(tendermintClient)
-				);
-			}
-		}
-	}
+        return new RegenApi(createTendermintConnection(tendermintClient));
+      }
+    }
+  }
 }
--- packages/demo-app/src/reportWebVitals.ts
+++ packages/demo-app/src/reportWebVitals.ts
@@ -1,17 +1,20 @@
-import { ReportHandler } from 'web-vitals';
+import {
+  ReportHandler,
+  getCLS,
+  getFID,
+  getFCP,
+  getLCP,
+  getTTFB,
+} from 'web-vitals';
 
 const reportWebVitals = (onPerfEntry?: ReportHandler): void => {
-	if (onPerfEntry && onPerfEntry instanceof Function) {
-		import('web-vitals')
-			.then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-				getCLS(onPerfEntry);
-				getFID(onPerfEntry);
-				getFCP(onPerfEntry);
-				getLCP(onPerfEntry);
-				getTTFB(onPerfEntry);
-			})
-			.catch(console.error);
-	}
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    getCLS(onPerfEntry);
+    getFID(onPerfEntry);
+    getFCP(onPerfEntry);
+    getLCP(onPerfEntry);
+    getTTFB(onPerfEntry);
+  }
 };
 
 export default reportWebVitals;
