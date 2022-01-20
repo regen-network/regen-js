de84bcc61fee27063fc4b7431195d3c5ec37c408
Mark Hagelberg <9438198+mhagel@users.noreply.github.com>
2022-01-20 11:24:54 -0700

    Update eslint configs (#27)
    - remove amaury/eslintrc
    - copy eslint config from regen-web

--- packages/api/e2e/api.spec.ts
+++ packages/api/e2e/api.spec.ts
@@ -4,21 +4,21 @@ import { QueryClientImpl } from '../src/generated/cosmos/bank/v1beta1/query';
 let api: RegenApi;
 
 describe('RegenApi with tendermint connection', () => {
-	beforeAll(async () => {
-		api = await RegenApi.connect({
-			connection: {
-				type: 'tendermint',
-				url: 'http://devnet.regen.network:26657',
-			},
-		});
-	});
+  beforeAll(async () => {
+    api = await RegenApi.connect({
+      connection: {
+        type: 'tendermint',
+        url: 'http://hambach.regen.network:26657',
+      },
+    });
+  });
 
-	it('should fetch balances using tendermint client', async () => {
-		const bankClient = new QueryClientImpl(api.connection.queryConnection);
-		const res = await bankClient.AllBalances({
-			address: 'regen:1j9h8dpu7ah2hl9rg7ycu0e64kh90rrlpk9kagz', // Amaury's account.
-		});
+  it('should fetch balances using tendermint client', async () => {
+    const bankClient = new QueryClientImpl(api.connection.queryConnection);
+    const res = await bankClient.AllBalances({
+      address: 'regen1df675r9vnf7pdedn4sf26svdsem3ugavgxmy46', // Test account.
+    });
 
-		expect(res.balances.length).toBeGreaterThanOrEqual(1);
-	});
+    expect(res.balances.length).toBeGreaterThanOrEqual(1);
+  });
 });
