cced6149633055537a1e4c6200d970f1cd4cf338
Mark Hagelberg <mark.hagelberg@gmail.com>
2022-09-11 00:16:28 -0600

    chore: successful MsgCreateClass test

--- packages/api/e2e/amino.spec.ts
+++ packages/api/e2e/amino.spec.ts
@@ -1,6 +1,9 @@
 import { DeliverTxResponse } from '@cosmjs/stargate';
 import { RegenApi } from '../src/api';
-import { MsgSend } from '../src/generated/regen/ecocredit/v1/tx';
+import {
+  MsgCreateClass,
+  MsgSend,
+} from '../src/generated/regen/ecocredit/v1/tx';
 import { StdFee } from '@cosmjs/amino/build/signdoc';
 import { Secp256k1HdWallet } from '@cosmjs/amino/build/secp256k1hdwallet';
 
@@ -16,27 +19,14 @@ const TEST_FEE: StdFee = {
   gas: '200000',
 };
 const TEST_MEMO = `regen-js v${process.env.npm_package_version} tests`;
-const TEST_MSG_SEND = MsgSend.fromPartial({
-  sender: TEST_ADDRESS,
-  recipient: TEST_ADDRESS,
-  credits: [
-    { batchDenom: 'C01-001-20170606-20210601-007', tradableAmount: '0.01' },
-  ],
-});
 
 const connect = async (): Promise<RegenApi> => {
   const mnemonic =
-    'time dice choose cabbage suit panic silly cattle picture auto grab hole'; //regen1m0qh5y4ejkz3l5n6jlrntxcqx9r0x9xjv4vpcp
+    'time dice choose cabbage suit panic silly cattle picture auto grab hole';
 
-  // Amino signer throws auth error:
   const signer = await Secp256k1HdWallet.fromMnemonic(mnemonic, {
     prefix: 'regen',
   });
-
-  // proto signer works:
-  // const signer = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic, {
-  //   prefix: 'regen',
-  // });
 
   return RegenApi.connect({
     connection: {
@@ -49,10 +39,20 @@ const connect = async (): Promise<RegenApi> => {
 
 describe('RegenApi with tendermint connection', () => {
   describe('Signing and broadcasting txs', () => {
-    it('should sign and broadcast using legacy amino sign mode', async () => {
+    it('should sign and broadcast MsgSend using legacy amino sign mode', async () => {
       let txRes: DeliverTxResponse | undefined;
       const api = await connect();
       const msgClient = api.msgClient;
+      const TEST_MSG_SEND = MsgSend.fromPartial({
+        sender: TEST_ADDRESS,
+        recipient: TEST_ADDRESS,
+        credits: [
+          {
+            batchDenom: 'C01-001-20170606-20210601-007',
+            tradableAmount: '0.01',
+          },
+        ],
+      });
 
       const signedTxBytes = await msgClient?.sign(
         TEST_ADDRESS,
@@ -63,10 +63,38 @@ describe('RegenApi with tendermint connection', () => {
 
       expect(signedTxBytes).toBeTruthy();
       if (signedTxBytes) {
-        // console.log('signedTxBytes', signedTxBytes);
         txRes = await msgClient?.broadcast(signedTxBytes);
-        console.log('txRes', txRes);
         expect(txRes).toBeTruthy();
+        expect(txRes?.code).toBe(0);
+      }
+    });
+    it('should sign and broadcast MsgCreateClass using legacy amino sign mode', async () => {
+      let txRes: DeliverTxResponse | undefined;
+      const api = await connect();
+      const msgClient = api.msgClient;
+      const TEST_MSG_SEND = MsgCreateClass.fromPartial({
+        admin: TEST_ADDRESS,
+        issuers: [TEST_ADDRESS],
+        metadata: 'unit test metadata',
+        creditTypeAbbrev: 'C',
+        fee: {
+          denom: 'uregen',
+          amount: '20000000',
+        },
+      });
+
+      const signedTxBytes = await msgClient?.sign(
+        TEST_ADDRESS,
+        [TEST_MSG_SEND],
+        TEST_FEE,
+        TEST_MEMO,
+      );
+
+      expect(signedTxBytes).toBeTruthy();
+      if (signedTxBytes) {
+        txRes = await msgClient?.broadcast(signedTxBytes);
+        expect(txRes).toBeTruthy();
+        expect(txRes?.code).toBe(0);
       }
     });
   });
