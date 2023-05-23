cf1a92cf41c5ade90d89d93d323070f1d8fafc8d
ryanchristo <12519942+ryanchristo@users.noreply.github.com>
2023-05-23 11:09:36 -0700

    fix lint

--- packages/api/scripts/aminos.js
+++ packages/api/scripts/aminos.js
@@ -1,129 +1,128 @@
 export const AMINO_MAP = {
-
-  // NOTE: amino types for cosmos and ibc modules included with telescope
+  // NOTE: amino types for cosmos and ibc modules are registered by telescope
 
   // regen.data.v1
   '/regen.data.v1.MsgAnchor': {
-    aminoType: 'regen-ledger/MsgAnchor'
+    aminoType: 'regen-ledger/MsgAnchor',
   },
   '/regen.data.v1.MsgAttest': {
-    aminoType: 'regen-ledger/MsgAttest'
+    aminoType: 'regen-ledger/MsgAttest',
   },
   '/regen.data.v1.MsgDefineResolver': {
-    aminoType: 'regen-ledger/MsgDefineResolver'
+    aminoType: 'regen-ledger/MsgDefineResolver',
   },
   '/regen.data.v1.MsgRegisterResolver': {
-    aminoType: 'regen-ledger/MsgRegisterResolver'
+    aminoType: 'regen-ledger/MsgRegisterResolver',
   },
 
   // regen.ecocredit.v1
   '/regen.ecocredit.v1.MsgCreateClass': {
-    aminoType: 'regen/MsgCreateClass'
+    aminoType: 'regen/MsgCreateClass',
   },
   '/regen.ecocredit.v1.MsgCreateProject': {
-    aminoType: 'regen/MsgCreateProject'
+    aminoType: 'regen/MsgCreateProject',
   },
   '/regen.ecocredit.v1.MsgCreateBatch': {
-    aminoType: 'regen/MsgCreateBatch'
+    aminoType: 'regen/MsgCreateBatch',
   },
   '/regen.ecocredit.v1.MsgMintBatchCredits': {
-    aminoType: 'regen/MsgMintBatchCredits'
+    aminoType: 'regen/MsgMintBatchCredits',
   },
   '/regen.ecocredit.v1.MsgSealBatch': {
-    aminoType: 'regen/MsgSealBatch'
+    aminoType: 'regen/MsgSealBatch',
   },
   '/regen.ecocredit.v1.MsgSend': {
-    aminoType: 'regen/MsgSend'
+    aminoType: 'regen/MsgSend',
   },
   '/regen.ecocredit.v1.MsgRetire': {
-    aminoType: 'regen/MsgRetire'
+    aminoType: 'regen/MsgRetire',
   },
   '/regen.ecocredit.v1.MsgCancel': {
-    aminoType: 'regen/MsgCancel'
+    aminoType: 'regen/MsgCancel',
   },
   '/regen.ecocredit.v1.MsgUpdateClassAdmin': {
-    aminoType: 'regen/MsgUpdateClassAdmin'
+    aminoType: 'regen/MsgUpdateClassAdmin',
   },
   '/regen.ecocredit.v1.MsgUpdateClassIssuers': {
-    aminoType: 'regen/MsgUpdateClassIssuers'
+    aminoType: 'regen/MsgUpdateClassIssuers',
   },
   '/regen.ecocredit.v1.MsgUpdateClassMetadata': {
-    aminoType: 'regen/MsgUpdateClassMetadata'
+    aminoType: 'regen/MsgUpdateClassMetadata',
   },
   '/regen.ecocredit.v1.MsgUpdateProjectAdmin': {
-    aminoType: 'regen/MsgUpdateProjectAdmin'
+    aminoType: 'regen/MsgUpdateProjectAdmin',
   },
   '/regen.ecocredit.v1.MsgUpdateProjectMetadata': {
-    aminoType: 'regen/MsgUpdateProjectMetadata'
+    aminoType: 'regen/MsgUpdateProjectMetadata',
   },
   '/regen.ecocredit.v1.MsgUpdateBatchMetadata': {
-    aminoType: 'regen/MsgUpdateBatchMetadata'
+    aminoType: 'regen/MsgUpdateBatchMetadata',
   },
   '/regen.ecocredit.v1.MsgBridge': {
-    aminoType: 'regen/MsgBridge'
+    aminoType: 'regen/MsgBridge',
   },
   '/regen.ecocredit.v1.MsgBridgeReceive': {
-    aminoType: 'regen/MsgBridgeReceive'
+    aminoType: 'regen/MsgBridgeReceive',
   },
   '/regen.ecocredit.v1.MsgAddCreditType': {
-    aminoType: 'regen/MsgAddCreditType'
+    aminoType: 'regen/MsgAddCreditType',
   },
   '/regen.ecocredit.v1.MsgSetClassCreatorAllowlist': {
-    aminoType: 'regen/MsgSetClassCreatorAllowlist'
+    aminoType: 'regen/MsgSetClassCreatorAllowlist',
   },
   '/regen.ecocredit.v1.MsgAddClassCreator': {
-    aminoType: 'regen/MsgAddClassCreator'
+    aminoType: 'regen/MsgAddClassCreator',
   },
   '/regen.ecocredit.v1.MsgRemoveClassCreator': {
-    aminoType: 'regen/MsgRemoveClassCreator'
+    aminoType: 'regen/MsgRemoveClassCreator',
   },
   '/regen.ecocredit.v1.MsgUpdateClassFee': {
-    aminoType: 'regen/MsgUpdateClassFee'
+    aminoType: 'regen/MsgUpdateClassFee',
   },
   '/regen.ecocredit.v1.MsgAddAllowedBridgeChain': {
-    aminoType: 'regen/MsgAddAllowedBridgeChain'
+    aminoType: 'regen/MsgAddAllowedBridgeChain',
   },
   '/regen.ecocredit.v1.MsgRemoveAllowedBridgeChain': {
-    aminoType: 'regen/MsgRemoveAllowedBridgeChain'
+    aminoType: 'regen/MsgRemoveAllowedBridgeChain',
   },
 
   // regen.ecocredit.basket.v1
   '/regen.ecocredit.basket.v1.MsgCreate': {
-    aminoType: 'regen.basket/MsgCreate'
+    aminoType: 'regen.basket/MsgCreate',
   },
   '/regen.ecocredit.basket.v1.MsgPut': {
-    aminoType: 'regen.basket/MsgPut'
+    aminoType: 'regen.basket/MsgPut',
   },
   '/regen.ecocredit.basket.v1.MsgTake': {
-    aminoType: 'regen.basket/MsgTake'
+    aminoType: 'regen.basket/MsgTake',
   },
   '/regen.ecocredit.basket.v1.MsgUpdateBasketFee': {
-    aminoType: 'regen.basket/MsgUpdateBasketFee'
+    aminoType: 'regen.basket/MsgUpdateBasketFee',
   },
   '/regen.ecocredit.basket.v1.MsgUpdateCurator': {
-    aminoType: 'regen.basket/MsgUpdateCurator'
+    aminoType: 'regen.basket/MsgUpdateCurator',
   },
   '/regen.ecocredit.basket.v1.MsgUpdateDateCriteria': {
-    aminoType: 'regen.basket/MsgUpdateDateCriteria'
+    aminoType: 'regen.basket/MsgUpdateDateCriteria',
   },
 
   // regen.ecocredit.marketplace.v1
   '/regen.ecocredit.marketplace.v1.MsgSell': {
-    aminoType: 'regen.marketplace/MsgSell'
+    aminoType: 'regen.marketplace/MsgSell',
   },
   '/regen.ecocredit.marketplace.v1.MsgUpdateSellOrders': {
-    aminoType: 'regen.marketplace/MsgUpdateSellOrders'
+    aminoType: 'regen.marketplace/MsgUpdateSellOrders',
   },
   '/regen.ecocredit.marketplace.v1.MsgCancelSellOrder': {
-    aminoType: 'regen.marketplace/MsgCancelSellOrder'
+    aminoType: 'regen.marketplace/MsgCancelSellOrder',
   },
   '/regen.ecocredit.marketplace.v1.MsgBuyDirect': {
-    aminoType: 'regen.marketplace/MsgBuyDirect'
+    aminoType: 'regen.marketplace/MsgBuyDirect',
   },
   '/regen.ecocredit.marketplace.v1.MsgAddAllowedDenom': {
-    aminoType: 'regen.marketplace/MsgAddAllowedDenom'
+    aminoType: 'regen.marketplace/MsgAddAllowedDenom',
   },
   '/regen.ecocredit.marketplace.v1.MsgRemoveAllowedDenom': {
-    aminoType: 'regen.marketplace/MsgRemoveAllowedDenom'
+    aminoType: 'regen.marketplace/MsgRemoveAllowedDenom',
   },
 };
